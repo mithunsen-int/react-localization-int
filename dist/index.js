e0983090d9fff564c542745edd337b307afce191
mithunsen-int <mithun.sen@indusnet.co.in>
2024-03-15 19:31:05 +0530

    Datatype declaration updated

--- dist/index.js
+++ dist/index.js
@@ -1,68 +1,50 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.useTranslation = exports.LocalizationProvider = void 0;
 var react_1 = require("react");
+/**
+ * Context for localization.
+ * @type {React.Context<LocalizationContextType | undefined>}
+ */
 var LocalizationContext = (0, react_1.createContext)(undefined);
+/**
+ * Localization provider component.
+ * @param {Props} props - Props for LocalizationProvider.
+ * @returns {React.ReactElement} A React component.
+ */
 var LocalizationProvider = function (_a) {
     var children = _a.children, _b = _a.defaultLocale, defaultLocale = _b === void 0 ? 'en' : _b, source = _a.source;
     var _c = (0, react_1.useState)(defaultLocale), locale = _c[0], setLocale = _c[1];
     var _d = (0, react_1.useState)({}), translations = _d[0], setTranslations = _d[1];
+    /**
+     * Function to change the locale.
+     * @param {string} locale - The new locale to set.
+     */
     var changeLocale = function (locale) {
         setLocale(locale);
     };
-    var loadTranslations = function (locale) { return __awaiter(void 0, void 0, void 0, function () {
-        return __generator(this, function (_a) {
-            try {
-                if (locale && (source === null || source === void 0 ? void 0 : source.hasOwnProperty(locale))) {
-                    setTranslations(source[locale]);
-                }
-                else {
-                    throw 'Provided locale does not exists in source!';
-                }
+    /**
+     * Function to load translations for the locale.
+     * @param {string} locale - The locale for which translations need to be loaded.
+     */
+    var loadTranslations = function (locale) {
+        try {
+            if (locale && (source === null || source === void 0 ? void 0 : source.hasOwnProperty(locale))) {
+                setTranslations(source[locale]);
             }
-            catch (error) {
-                console.log("Failed to load translations for locale: '".concat(locale, "'."), error);
-                return [2 /*return*/, {}];
+            else {
+                throw new Error('Provided locale does not exist in source!');
             }
-            return [2 /*return*/];
-        });
-    }); };
+        }
+        catch (error) {
+            console.log("Failed to load translations for locale: '".concat(locale, "'."), error);
+        }
+    };
+    /**
+     * Function to translate a key.
+     * @param {string} key - The key to translate.
+     * @returns {string} The translated string.
+     */
     var t = function (key) { return translations[key] || key; };
     (0, react_1.useEffect)(function () {
         (function () {
@@ -75,6 +57,10 @@ var LocalizationProvider = function (_a) {
     return (react_1.default.createElement(LocalizationContext.Provider, { value: { t: t, locale: locale, changeLocale: changeLocale } }, children));
 };
 exports.LocalizationProvider = LocalizationProvider;
+/**
+ * Hook to use translations in components.
+ * @returns {LocalizationContextType} Localization context.
+ */
 var useTranslation = function () {
     var context = (0, react_1.useContext)(LocalizationContext);
     if (context === undefined) {
