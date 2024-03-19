ad955c09c7c0f8d33968662e822c70bba1345050
mithunsen-int <mithun.sen@indusnet.co.in>
2024-03-19 18:33:18 +0530

    Added an example folder

---
+++ example/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useTranslation } from './LocalizationProvider';
+
+export const Home = () => {
+    const { t, changeLocale, locale } = useTranslation();
+    return (
+        <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+            <TouchableOpacity style={{marginBottom: 50, padding: 5, backgroundColor: '#30a3dc', borderWidth: 1, borderColor: '#0b7970', borderRadius: 5}} onPress={() => changeLocale((locale == 'en') ? 'fr' : 'en')}>
+                <Text style={{color: '#fff'}}>Change Language</Text>
+            </TouchableOpacity>
+            <Text>{t('greeting')}</Text>
+            <Text>{t('welcome')}</Text>
+            <Text>{t('home.title')}</Text>
+        </View>
+    )
+}
